5e39df5a86da0cee34c4ef219865468228c1a2bd
Agnieszka Kaminska <zabula81@o2.pl>
2023-12-20 14:20:20 +0100

    frist

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -1,4 +1,37 @@
-const a = '4.2';
+const a = "4.2";
 const b = 9;
 
-console.log(a, b);
+const aa = Number(a);
+const bb = Number(b);
+console.log(a, b, aa, bb);
+
+const typeA = typeof a;
+const typeB = typeof b;
+const typeAA = typeof aa;
+const typeBB = typeof bb;
+console.log(
+  `typ a: ${typeA}, typ b: ${typeB}, typ aa: ${typeAA}, typ bb: ${typeBB}`
+);
+
+const add = aa + bb;
+const sub = aa - bb;
+const multi = aa * bb;
+const divi = aa / bb;
+const rest = aa % bb;
+
+console.log(`a+b = ${add}`);
+console.log(`a-b = ${sub}`);
+console.log(`a*b = ${multi}`);
+console.log(`a/b = ${divi}`);
+console.log(`a%b = ${rest}`);
+
+const arr = [add, sub, multi, divi, rest];
+console.log(arr);
+
+arr.forEach((item) => {
+  if (item > 20) {
+    console.log("20 <",item);
+  } else {
+    console.log("20 >", item);
+  }
+});
