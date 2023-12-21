56b816e6547707fe5fca209f31ee516776cf5336
Agnieszka Kaminska <zabula81@o2.pl>
2023-12-21 12:38:31 +0100

    second

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,52 @@
+// /* rozwiązanie z pętlą for */
+const x = prompt("Podaj Liczbę do mnożenie w przedziale od 1 - 9");
+const num = parseInt(x);
 
-/* rozwiązanie z pętlą for */
-const x = 4;
+const validateNum = checkPrompt(num);
+// const x = 4;
+function multiTable(num) {
+  for (let i = 1; i <= 9; i++) {
+    const multi = i * num;
+    console.log(`${num} X ${i} = ${multi}`);
+  }
+}
 
-
+multiTable(validateNum);
 
 /* rozwiązanie z pętlą while  */
+
+const a = prompt("Podaj podstawę potęgowanie (1 - 9)");
+const n = prompt("Podaj wykłądnik potęgowanie (1 - 9)");
+const base = parseInt(a);
+const exponent = parseInt(n);
+
+const validBase = checkPrompt(base);
+const validExponent = checkPrompt(exponent);
+
+function power(a, n) {
+  let iteration = 0;
+  let str = "";
+
+  while (iteration < n) {
+    iteration++;
+    if (iteration === 1) {
+      str += a;
+    } else {
+      str += ` * ${a}`;
+    }
+  }
+  const result = a ** n;
+
+  console.log(`Wynik potęgowania ${str} = ${result}`);
+}
+
+power(validBase, validExponent);
+
+function checkPrompt(num) {
+  if (!isNaN(num) && num >= 1 && num <= 9) {
+    return num;
+  } else {
+    num = parseInt(prompt("Twoja liczba musi być w przedziale od 1 do 9"));
+    return checkPrompt(num);
+  }
+}
